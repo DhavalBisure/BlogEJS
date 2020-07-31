82d8f21e0fbba7dc4ff55bdec8a970787bd99364
DhavalBisure <dhavalbisure98@gmail.com>
2020-07-31 23:16:31 +0530

    Add compose page

--- app.js
+++ app.js
@@ -27,6 +27,17 @@ app.get("/contact",function(req,res){
     res.render("contact",{contactContent:contactContent});
 });
 
+app.get("/compose",function(req,res){
+    res.render("compose");
+});
+
+app.post("/compose",function(req,res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  console.log(post);
+});
 
 
 
